bbcbd28222a3148af8e6eb7718f35b0f2b673306
JasonSorria <sijing06222@163.com>
2023-03-06 15:44:20 +0800

    optimized setup scripts

--- linux/challenge-cancel-file-change/setup.sh
+++ linux/challenge-cancel-file-change/setup.sh
@@ -4,5 +4,7 @@ mkdir myrepo
 cd myrepo
 echo "Hello, world!" > myfile.txt
 git init
+git config --global user.email "sijing06222@163.com"
+git config --global user.name "thenicebro"
 git add myfile.txt
 git commit -m "Initial commit"
