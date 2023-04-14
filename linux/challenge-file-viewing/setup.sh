9bcbb438d03cbcac49a95e9ff0273e7d433a55e8
JasonSorria <sijing06222@163.com>
2023-04-14 12:35:22 +0800

    fiexd

--- linux/challenge-file-viewing/setup.sh
+++ linux/challenge-file-viewing/setup.sh
@@ -6,7 +6,7 @@ echo "This is line 3." >> example.txt
 echo "This is line 4." >> example.txt
 echo "This is line 5." >> example.txt
 
-echo "This is an example file.
+echo "This is an example file.>> step22.txt
 echo "It contains some text for the Linux challenge." >> step22.txt
 
 echo "This is line 5." >> step33.txt
