a394bfae77ebf11d873a1c0fe27086d1dda21bc7
JasonSorria <sijing06222@163.com>
2023-04-14 14:23:32 +0800

    optimize verify.sh

--- linux/challenge-stream-editor-sed/verify3.sh
+++ linux/challenge-stream-editor-sed/verify3.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
-result=`diff result2.txt /tmp/verify3.txt | wc -l`
-if [ $result -eq 0 ];then
-	exit 0
+if [ -f result2.txt ];then
+	result=`diff result2.txt /tmp/verify3.txt | wc -l`
+	if [ $result -eq 0 ];then
+		exit 0
+	else
+		exit 1
+	fi
 else
 	exit 1
 fi
