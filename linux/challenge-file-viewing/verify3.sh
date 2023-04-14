71f4092fed4362d6293a4936867532810c927c0b
JasonSorria <sijing06222@163.com>
2023-04-14 12:48:11 +0800

    optimize verify.sh

--- linux/challenge-file-viewing/verify3.sh
+++ linux/challenge-file-viewing/verify3.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
-result=`diff step33.txt step3.txt | wc -l`
-if [ $result -eq 0 ];then
-	exit 0
+if [ -f step3.txt];then
+	result=`diff step33.txt step3.txt | wc -l`
+	if [ $result -eq 0 ];then
+		exit 0
+	else
+		exit 1
+	fi
 else
 	exit 1
 fi
