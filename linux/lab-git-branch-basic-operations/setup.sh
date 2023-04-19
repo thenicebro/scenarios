50c9a8fb672c80082f6c6eb2e6aff49b9754c52d
JasonSorria <sijing06222@163.com>
2023-04-19 14:50:14 +0800

    optimized setup.sh

--- linux/lab-git-branch-basic-operations/setup.sh
+++ linux/lab-git-branch-basic-operations/setup.sh
@@ -5,10 +5,13 @@ cd ~/myrepo
 git init
 git config --global user.email "labex@example.com"
 git config --global user.name "lab"
-git branch old-name
-git branch branch-to-delete
-git checkout old-name
 touch a.txt
 git add a.txt
 git commit -m 'add a.txt'
+git branch old-name
+git branch branch-to-delete
+git checkout old-name
+touch b.txt
+git add b.txt
+git commit -m 'add b.txt'
 git checkout master
