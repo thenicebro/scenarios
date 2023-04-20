67d9892dba8303e59d0067b46b2eb8f4c8efb3ae
JasonSorria <sijing06222@163.com>
2023-04-20 11:15:46 +0800

    fixed: setup.sh

--- linux/challenge-file-printing/setup.sh
+++ linux/challenge-file-printing/setup.sh
@@ -5,7 +5,7 @@ mkdir ~/project/myrepo
 
 touch ~/project/myrepo/file1.sh ~/project/myrepo/file2.bat ~/project/myrepo/file3.txt
 mkdir ~/project/myrepo/dir1 ~/project/myrepo/dir2
-touch ~/project/myrepo/dir1/file4.sh ~/myrepo/dir2/file5.exe
+touch ~/project/myrepo/dir1/file4.sh ~/project/myrepo/dir2/file5.exe
 
 echo "This is the first line." >> ~/project/example.txt
 echo "This is the second line." >> ~/project/example.txt
