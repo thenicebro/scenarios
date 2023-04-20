10fde3e533537d2ca9e840e6921f01197b2df52f
JasonSorria <sijing06222@163.com>
2023-04-20 11:34:10 +0800

    fixed: setup.sh

--- linux/challenge-special-operations-with-files/setup.sh
+++ linux/challenge-special-operations-with-files/setup.sh
@@ -3,5 +3,5 @@
 mkdir ~/project/myrepo
 mkdir ~/project/myrepo/dir1 ~/project/myrepo/dir2
 touch ~/project/myrepo/file1.txt ~/project/myrepo/file2.txt
-echo "this is line 1" > ~/project/myrepo/a\\.\ a.txt
-echo "this is line 1" > /tmp/verify.txt
+echo "this is line 1..." > ~/project/myrepo/a\\.\ a.txt
+echo "this is line 1..." > /tmp/verify.txt
