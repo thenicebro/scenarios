be18db4b7b4cd7a12479025eea5eb7049960516b
JasonSorria <sijing06222@163.com>
2023-05-13 18:13:58 +0800

    fix

--- linux/challenge-directories-lists/verify1.sh
+++ linux/challenge-directories-lists/verify1.sh
@@ -2,14 +2,13 @@
 
 cd ~/project
 
-touch ~/tmp/output.txt
-bash dirList.sh ~/project ~/project/dir > ~/tmp/output.txt
+bash dirList.sh ~/project ~/project/dir > /tmp/output.txt
 
-rs=$(cat ~/tmp/output.txt | egrep "*.sh" | wc -l)
+rs=$(cat /tmp/output.txt | egrep "*.sh" | wc -l)
 if [ $rs -eq 2 ];then
-	rm ~/tmp/output.txt
+	rm /tmp/output.txt
 	exit 0
 else
-	rm ~/tmp/output.txt
+	rm /tmp/output.txt
 	exit 1
 fi
